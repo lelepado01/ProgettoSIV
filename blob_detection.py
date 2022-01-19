b01381baf860a5719bbbf8032c564334b6c9efbb
Gabriele Padovani <gabriele.padovani@gmail.com>
2022-01-19 16:11:59 +0100

    interpolation funginterpolation fungee

--- blob_detection.py
+++ blob_detection.py
@@ -1,13 +1,42 @@
 
 import numpy as np
 import cv2
+from scipy import interpolate
+
+
+def calculate_curve(pts, elements_to_remove = 0): 
+    if len(pts) < 3: 
+        return []
+        
+    x_list = [item[0] for item in pts]
+    y_list = [item[1] for item in pts]
+
+    for _ in range(elements_to_remove): 
+        del x_list[-1]
+        del y_list[-1]
+
+    f = interpolate.interp1d(x_list, y_list, kind='quadratic', fill_value='extrapolate')
+
+    x_min = min(x_list)
+    x_max = max(x_list)
+    left_range = 150
+    right_range = 50
+    x_points = np.arange(x_min - left_range, x_max + right_range, 0.1)
+
+    ls = []
+    for x_pt in x_points: 
+        ls.append((x_pt, f(x_pt)))
+
+    return ls
+
 
 algorithm_view = False
   
-cap = cv2.VideoCapture('video/ft2.mp4')
+video_path = 'video/ft5.mp4'
+cap = cv2.VideoCapture(video_path)
   
 # initializing subtractor 
-fgbg = cv2.createBackgroundSubtractorMOG2(0,80) 
+fgbg = cv2.createBackgroundSubtractorMOG2(history=30) 
 
 paused = False
 
@@ -15,39 +44,46 @@ pts_list = []
 
 params = cv2.SimpleBlobDetector_Params()
 params.filterByArea = True
-params.minArea = 150
+params.minArea = 490
 params.filterByCircularity = True
 params.minCircularity = 0.8
 params.filterByConvexity = True
 params.minConvexity = 0.2
 params.filterByInertia = True
 params.minInertiaRatio = 0.1
-params.filterByColor = True
 detector = cv2.SimpleBlobDetector_create(params)
 
 
 while(1):
     if not paused: 
         ret, frame = cap.read()       
+        if not ret: 
+            break
 
         # img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        # fgmask = fgbg.apply(frame)  
+        #fgmask = fgbg.apply(frame)  
         fgmask = frame  
         kernel = np.ones((8,3), np.uint8)
-        fgmask = cv2.erode(fgmask, kernel)
-        fgmask = cv2.dilate(fgmask, kernel)
+        # fgmask = cv2.erode(fgmask, kernel)
+        # fgmask = cv2.dilate(fgmask, kernel)
 
         keypoints = detector.detect(fgmask)
-
         blank = np.zeros((1, 1))
     
-        pts_list.append(keypoints)
+        if keypoints != (): 
+            pts_list.append(keypoints[0].pt)
 
         if algorithm_view: 
             frame = fgmask
+
+        for (x, y) in calculate_curve(pts_list):  
+            cv2.circle(frame, (int(x), int(y)), 1, (255,0,255), 4)
+
         for keypoint in pts_list:
-            blobs = cv2.drawKeypoints(frame, keypoint, blank, (255, 0, 0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            frame = cv2.putText(blobs, "", (20, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 100, 255), 2)
+            if keypoint == ():  
+                continue
+            (x, y) = keypoint
+            cv2.circle(frame, (int(x), int(y)), 1, (0,255,255), 5)
 
         cv2.imshow('frame', frame)
 
@@ -57,6 +93,20 @@ while(1):
 
     if k == 32:
         paused = not paused
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+
+for (x, y) in calculate_curve(pts_list, 5):  
+    cv2.circle(frame, (int(x), int(y)), 1, (255,0,255), 4)
+
+for (x, y) in pts_list:  
+    cv2.circle(frame, (int(x), int(y)), 1, (0,255,255), 5)
+
+
+cv2.imshow('Final Interpolated function', frame)
+cv2.waitKey()
+
   
 cap.release()
 cv2.destroyAllWindows()
--- motion_tracking.py
+++ motion_tracking.py
@@ -1,73 +1,90 @@
 
 import numpy as np
 import cv2
-  
-cap = cv2.VideoCapture('video/ft3.mp4')
+from scipy import interpolate
+
+def calculate_curve(pts, elements_to_remove = 0): 
+    if len(pts) < 3: 
+        return []
+        
+    x_list = [item[0] for item in pts]
+    y_list = [item[1] for item in pts]
+
+    for _ in range(elements_to_remove): 
+        del x_list[-1]
+        del y_list[-1]
+
+    f = interpolate.interp1d(x_list, y_list, kind='quadratic', fill_value='extrapolate')
+
+    x_min = min(x_list)
+    x_max = max(x_list)
+    left_range = 150
+    right_range = 50
+    x_points = np.arange(x_min - left_range, x_max + right_range, 0.1)
+
+    ls = []
+    for x_pt in x_points: 
+        ls.append((x_pt, f(x_pt)))
+
+    return ls
+
+
+cap = cv2.VideoCapture('video/ft5.mp4')
 
 fgbg = cv2.createBackgroundSubtractorMOG2(0,80) 
 
 paused = False
 
-keypoints = []
+pts_list = []
 
 params = cv2.SimpleBlobDetector_Params()
 params.filterByArea = True
-params.minArea = 250
+params.minArea = 500
 params.filterByCircularity = True
 params.minCircularity = 0.8
 params.filterByConvexity = True
-params.minConvexity = 0.1
+params.minConvexity = 0.01
 params.filterByInertia = True
 params.minInertiaRatio = 0.01
 
 detector = cv2.SimpleBlobDetector_create(params)
 
-while(len(keypoints) == 0): 
+initial_keypoint = []
+while(len(initial_keypoint) == 0): 
     ret, frame = cap.read()
     if not ret:
-        print("Palla non trovata")
+        print("---\nBall not found\n---")
         exit() 
-    keypoints = detector.detect(frame)
+    initial_keypoint = detector.detect(frame)
 
-tracker  = cv2.TrackerCSRT_create()
+tracker = cv2.TrackerCSRT_create()
 
-for point in keypoints: 
-    (x, y) = point.pt
-    size = point.size * 3
-    # print(size)
-    area = (int(x - size), int(y -size), int(size), int(size))#(910, 247, 120, 76)
-print(area)
-# bboxs = cv2.selectROI(frame)
-# print(bboxs)
-# for bbox in bboxs:
-#     print(type(bbox))
+(x, y) = initial_keypoint[0].pt
+size = initial_keypoint[0].size * 3
+area = (int(x - size / 2), int(y -size / 2), int(size), int(size))
 
 # area = cv2.selectROI(frame)
-fgmask = fgbg.apply(frame)
-ret = tracker.init(fgmask, area)
+ret = tracker.init(frame, area)
 
-# ret, bbox = tracker.update(fgmask)
-
-# if ret:
-#     (x, y, w, h) = bbox 
-#     cv2.rectangle(fgmask, (x,y), (w + x, h + y), (0, 255, 0), 2, 1)
-
-
-# cv2.imshow('frame', fgmask)
-
-# cv2.waitKey()
-
-while(1):
+while True:
     if not paused: 
         ret, frame = cap.read()
-        fgmask = fgbg.apply(frame)
         if not ret:
             break
-        ret, bbox = tracker.update(fgmask)
+
+        ret, bbox = tracker.update(frame)
 
         if ret:
             (x, y, w, h) = [int(v) for v in bbox]
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2, 1)
+            pts_list.append((x+w/2, y + h/2))
+
+        for (x, y) in calculate_curve(pts_list):  
+            cv2.circle(frame, (int(x), int(y)), 1, (255,0,255), 4)
+
+        for bbox in pts_list: 
+            (x, y) = bbox
+            blank = np.zeros((1, 1))
+            cv2.circle(frame, (int(x), int(y)), 1, (0,255,255), 5)
 
         cv2.imshow('frame', frame)
 
@@ -77,6 +94,18 @@ while(1):
 
     if k == 32:
         paused = not paused
-  
+
+cap = cv2.VideoCapture('video/ft5.mp4')
+ret, frame = cap.read()
+
+for (x, y) in calculate_curve(pts_list, 10):  
+    cv2.circle(frame, (int(x), int(y)), 1, (255,0,255), 4)
+
+for (x, y) in pts_list:  
+    cv2.circle(frame, (int(x), int(y)), 1, (0,255,255), 5)
+
+cv2.imshow('Final Interpolated function', frame)
+cv2.waitKey()
+
 cap.release()
 cv2.destroyAllWindows()
